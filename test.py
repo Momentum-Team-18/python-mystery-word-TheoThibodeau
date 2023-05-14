2a9338ec51b5835b148b521d7c5d99037327a9fe
TheoThibodeau <teddythibodeau@gmail.com>
2023-05-14 10:25:10 -0400

    Difficulty working

--- test.py
+++ test.py
@@ -5,9 +5,36 @@ def play_game(filename):
 
     with open(filename) as file:
         word_list = file.read().split()
-
-    random_word = random.choice(word_list)
-    print(random_word)
+        easy_mode = []
+        normal_mode = []
+        hard_mode = []
+        # define lists for each type of word difficulty
+        
+        for word in word_list:
+            if len(word) >= 4 and len(word) <= 6:
+                easy_mode.append(word)
+            elif len(word) >= 6 and len(word) <= 8:
+                normal_mode.append(word)
+            else:
+                hard_mode.append(word)
+        # write a loop that iterates the words and organizes them based on length
+        
+        easy_word = random.choice(easy_mode)
+        normal_word = random.choice(normal_mode)
+        hard_word = random.choice(hard_mode)
+        # assign difficulty to chose word
+        choose_level = str(input("Choose level of difficulty (Easy, Normal, or Hard): "))
+        
+        if choose_level == "Easy":
+            random_word = easy_word
+        if choose_level == "Normal":
+            random_word = normal_word
+        if choose_level == "Hard":
+            random_word = hard_word
+        # define different levels of difficulty
+    
+    # random_word = random.choice(word_list)
+    # print(random_word)
     
     display = []
     num_letters = len(random_word)
@@ -16,12 +43,13 @@ def play_game(filename):
         display.append('_')
     print(display)    
 
-    guesses = 3
+    guesses = 8
+
     while guesses > 0:
         
         guess = input("guess a letter: ")
         if guess in random_word:
-            print(f"correct: {letter}")
+            print(f"correct: {guess}")
             for i in range(len(random_word)):
                 if guess == random_word[i]:
                     display[i] = guess
